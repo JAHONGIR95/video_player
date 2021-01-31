90cc0e180b6c099a234b09302f3c36b723c16327
Jahongir95 <sjahongir0032@gmail.com>
2021-01-31 23:54:34 +0500

    video duration is given to item

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,8 @@ var toggle = document.querySelector('.toggle');
 var skipButtons = document.querySelectorAll('[data-skip]');
 var ranges = document.querySelectorAll('.player__slider');
 var volumeIcon = document.querySelector('#volume-icon');
+var soat = document.querySelector('#time');
+
 
 
 window.addEventListener('load', function () {
@@ -15,6 +17,9 @@ window.addEventListener('load', function () {
 function togglePlay() {
     var method = video.paused ? 'play' : 'pause';
     video[method]();
+    timeMetr(Math.round(video.duration));
+    console.log(video.currentTime);
+    
 }
 
 function updateButton() {
@@ -50,6 +55,36 @@ function range() {
     video[this.name] = this.value;
     volumeIconSwitcher(video.volume);
     // console.log(this.name);
+}
+
+function timeMetr(time){
+
+    a = Math.floor(time / 3600);
+    time = time % 3600;
+    b = Math.floor(time / 60);
+    time = time % 60;
+    c = time
+    
+    soat.innerHTML = `<span id="a-nol">0</span> ${a} : <span id="b-nol"> 0</span> ${b} : <span id="c-nol"> 0</span> ${c}`;
+
+    if(a >= 10){
+        document.querySelector('#a-nol').style.display = 'none';
+    }
+    else if(a == 0){
+        document.querySelector('#a-nol').textContent = 0;
+    }
+    if(b >= 10){
+        document.querySelector('#b-nol').style.display = 'none';
+    }
+    else if(b == 0){
+        document.querySelector('#b-nol').textContent = 0;
+    }
+    if(c >= 10){
+        document.querySelector('#c-nol').style.display = 'none';
+    }
+    else if(c == 0){
+        document.querySelector('#c-nol').textContent = 0;
+    }
 }
 
 video.addEventListener('click', togglePlay);
